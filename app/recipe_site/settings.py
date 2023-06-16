7e16fd7e16a6fd5a328d77a4d4fcf632eb8e0f7e
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-06-16 13:17:36 +0900

    [update]アプリケーションの作成

--- app/recipe_site/settings.py
+++ app/recipe_site/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
+    'recommend.apps.RecommendConfig',
+    'recipe.apps.RecipeConfig'
 ]
 
 MIDDLEWARE = [
--- app/recipe_site/urls.py
+++ app/recipe_site/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('login/', include('login.urls')),
+    # path('recommend/', include('recommend.urls')),
+    # path('recipe/', include('recipe.urls')),
 ]
