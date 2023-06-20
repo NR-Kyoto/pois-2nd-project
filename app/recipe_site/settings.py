7941c9a752d28ac6f10c80001724b79f1ad75867
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-06-20 14:47:00 +0900

    [update]未ログインでのページアクセスを制限

--- app/recipe_site/settings.py
+++ app/recipe_site/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    # ログインしていない状態でのアクセスを防止
+    'recipe_site.middleware.auth.AuthMiddleware',
 ]
 
 ROOT_URLCONF = 'recipe_site.urls'
