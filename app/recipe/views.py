35bdb0f73c7ce3d9418d9d23beebf65845c36ada
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-07-21 21:30:48 +0900

    [update]limit

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -32,8 +32,8 @@ class MergeRecipes(APIView):
             return Response({"error": "No recipes"}, status=HTTP_406_NOT_ACCEPTABLE)
 
         try:
-            # scheduler = RecipeScheduler(user=request.user, dishes=request.data['recipes'], limit_time=20)
-            scheduler = RecipeScheduler(user=request.user, dishes=request.data['recipes'])
+            scheduler = RecipeScheduler(user=request.user, dishes=request.data['recipes'], limit_time=20)
+            # scheduler = RecipeScheduler(user=request.user, dishes=request.data['recipes'])
             schedule = scheduler.scheduling()
 
             del scheduler 
