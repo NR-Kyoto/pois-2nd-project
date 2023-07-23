56213c876a2c33f0155355304fc7b1dfb591ad10
YouthOnishi <onishi.yusuke.64e@st.kyoto-u.ac.jp>
2023-07-23 13:09:57 +0900

    commit

--- templates/src/views/Search.vue
+++ templates/src/views/Search.vue
@@ -1,78 +1,96 @@
 <template>
   <div>
-    <v-row>
-      <v-col cols="12" lg="12" xl="8">
-        <div>
-          <div>
-            <div>
-              <h2 class="text-h4 font-weight-bold">[The number of candidates] recipes found</h2>
-              <!--h4 class="text-h6">Some category description goes here</h4-->
-            </div>
+    <div class="pt-16"></div>
+    <!-- 検索フォーム -->
+    <v-text-field
+      v-model="searchKeyword"
+      label="何が食べたい？"
+      outlined
+      class="mb-4"
+    >
+    </v-text-field>
 
-            <v-divider class="my-4"></v-divider>
+    <v-btn color="primary" @click="searchRecipes">検索</v-btn>
 
-            <v-row>
-              <v-col cols="12" md="6" lg="4" v-for="i in 18" :key="i">
-                <v-hover
-                  v-slot:default="{ hover }"
-                  open-delay="50"
-                  close-delay="50"
-                >
-                  <div>
-                    <v-card
-                      flat
-                      :color="hover ? 'white' : 'transparent'"
-                      :elevation="hover ? 12 : 0"
-                      hover
-                      to="/detail"
-                    >
-                      <v-img
-                        src="https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_1280.jpg"
-                        :aspect-ratio="16 / 9"
-                        gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)"
-                        height="200px"
-                        class="elevation-2"
-                        style="border-radius: 16px"
-                      >
-                        <v-card-text>
-                          <!--v-btn color="accent">ANIMAL</v-btn-->
-                          <div class="text-h5 font-weight-bold white--text">
-                          Recipe image must be here
-                          </div>
-                        </v-card-text>
-                      </v-img>
+    <div class="pt-16"></div>
+    <!-- 検索結果表示 -->
+    <div v-if="recommendRecipes.length > 0">
+      <h2>おすすめレシピ</h2>
+      <v-card v-for="recipe in recommendRecipes" :key="recipe.id">
+        <v-img
+          :aspect-ratio="16 / 9"
+          class="elevation-2"
+          gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)"
+          height="200px"
+          :src="recipe.image"
+          style="border-radius: 16px"
+        >
+        <v-card-text>
+          <v-btn color="secondary" to="category">More</v-btn>
+        </v-card-text>
+        </v-img>
 
-                      <v-card-text>
-                        <div class="text-h5 font-weight-bold primary--text">
-                          Recipe title must be here
-                        </div>
-
-                        <div class="text-body-1 py-4">
-                          Main ingredients / Cooking time
-                        </div>
-
-                        <div class="d-flex align-center">
-                          <v-avatar color="accent" size="36">
-                            <v-icon dark>mdi-feather</v-icon>
-                          </v-avatar>
-
-                          <div class="pl-2">Contributer name · Posted date</div>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </div>
-                </v-hover>
-              </v-col>
-            </v-row>
+        <v-card-text>
+          <div class="text-h5 font-weight-bold primary--text">
+            {{ recipe.dish.name }}
           </div>
-        </div>
-      </v-col>
-    </v-row>
+
+          <!-- 材料の表示 -->
+          <div class="text-body-1 py-4">
+            {{ recipe.ingredients }}
+          </div>
+
+          <div class="d-flex align-center">
+            <div class="mr-auto pl-2">
+              <v-icon>mdi-timer-alert-outline</v-icon>&nbsp;{{ recipe.time }} 分
+            </div>
+            <div class="p2">
+              <v-btn color="secondary" size="small" @click="hold(recipe)"
+              ><v-icon dark>mdi-plus</v-icon></v-btn
+              >
+            </div>
+          </div>
+        </v-card-text>
+      </v-card>
+    </div>
+    <div v-else>
+      <p>該当するレシピが見つかりません。</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "Search",
+  data() {
+    return {
+      searchKeyword: "", // 検索キーワードを保持するデータ
+      recipes: [], // 全てのレシピデータ
+      recommendRecipes: [], // おすすめレシピの結果を格納する配列
+    };
+  },
+  methods: {
+    async getRecommendations() {
+      try {
+        // ここでバックエンドのAPIエンドポイントにリクエストを送信 
+        const response = await axios.get("/api/recommend_recipe/", {
+          params: { keyword: this.searchKeyword },
+        });
+
+        // レスポンスからおすすめレシピの情報を取得してデータを更新
+        this.recommendRecipes = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    searchRecipes(){
+      // 検索フォームの入力が変更されたら、推薦レシピを取得する関数を呼び出す
+      this.getRecommendations();
+    },
+  },
 };
 </script>
+
